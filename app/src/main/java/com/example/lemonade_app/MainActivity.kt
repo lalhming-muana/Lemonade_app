be70cf52072e6b56809e2ddaf0831046f3b27b8b
lhmuana <edd_vai@yahoo.co.in>
2023-09-14 21:15:18 +0530

    image is clickable and changes

--- app/src/main/java/com/example/lemonade_app/MainActivity.kt
+++ app/src/main/java/com/example/lemonade_app/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,6 +12,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -37,16 +42,27 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(modifier: Modifier = Modifier) {
+
+    var result by remember { mutableStateOf(1) }
+    val imageResource = when(result){
+        1-> R.drawable.lemon_tree
+        2-> R.drawable.lemon_squeeze
+        3-> R.drawable.lemon_drink
+        else-> R.drawable.lemon_restart
+    }
+
+
     Column(Modifier.fillMaxSize(),
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally) {
 
         Image(
-            painter = painterResource(R.drawable.lemon_tree),
-            contentDescription = null
+            painter = painterResource(id =imageResource),
+            contentDescription = null,
+            modifier = Modifier.clickable{result+=1}
         )
         Text(
-            text = "Lemon tree",
+            text = "Tap the lemon tree to select a lemon",
             modifier = modifier
         )
     }
